506afa33ce8d9b4fe5c30a0ccddc2430fbec580a
sharmaJi <prakashdeep2902@gmail.com>
2025-01-05 16:04:49 +0530

    how to find out run time of your system

--- ModuleInNodeJS/osModule/os.js
+++ ModuleInNodeJS/osModule/os.js
@@ -1,13 +1,19 @@
-import os from "os";
-const upTimeInsec = os.uptime();
-console.log(`os up time in second ${upTimeInsec}`);
-const upTimeInmin = upTimeInsec / 60;
-console.log(`os up time in min ${upTimeInmin}`);
-const upTimeInhours = upTimeInmin / 60;
-console.log(`os up time in hours ${upTimeInhours}`);
-const upTimeIndays = upTimeInhours / 60;
-console.log(`os up time in days ${upTimeIndays}`);
+import os from "os"; // ES6 import
+
+const upTimeInSec = os.uptime();
+console.log(`OS uptime in seconds: ${upTimeInSec}`);
+
+const upTimeInMin = upTimeInSec / 60;
+console.log(`OS uptime in minutes: ${upTimeInMin}`);
+
+const upTimeInHours = upTimeInMin / 60;
+console.log(`OS uptime in hours: ${upTimeInHours}`);
+
+const upTimeInDays = upTimeInHours / 24; // Corrected to divide by 24 for days
+console.log(`OS uptime in days: ${upTimeInDays.toFixed(2)}`);
+
 const memInBytes = os.totalmem();
-console.log(`total memo of system in bytes ${memInBytes}`);
+console.log(`Total memory of system in bytes: ${memInBytes}`);
+
 const memInGb = memInBytes / 1024 ** 3;
-console.log(`total memo of system in GB ${memInGb.toFixed(2)}`);
+console.log(`Total memory of system in GB: ${memInGb.toFixed(2)}`);
